4806bd034fb380727b69133e6dcdc0e3d5033ba3
OMUX-KUT <hawk.jyunki@gmail.com>
2026-09-17 01:59:23 +0900

    Initialize kut_geometry_engine package with version info

---
+++ kut_geometry_engine/__init__.py
@@ -0,0 +1,9 @@
+"""
+KUT Geometry Engine
+Package for Test-Time Search Optimization via Geometric Ricci Annealing
+"""
+
+from .multi_attractor import MultiAttractorKUTController
+
+__version__ = "1.1.0"
+__all__ = ["MultiAttractorKUTController"]
